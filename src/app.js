eecfb5f3fe0d10fc6c402a47619d67ae5e18eb0b
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-04 15:46:32 +0300

    "Updated package.json, removed public/about.html and public/index.html, updated src/app.js, and removed views/index.hbs"

--- src/app.js
+++ src/app.js
@@ -5,12 +5,32 @@ const app = express();
 const port = 8080;
 
 const publicDirectory = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
 app.use(express.static(publicDirectory));
 
-app.get("/", (req, res) => {
-  res.send();
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Vincent Anjiri",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Vincent Anjiri",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Get Help",
+    getHelp: "Visit our offices for assistance",
+  });
 });
 
 app.listen(port, () => {
