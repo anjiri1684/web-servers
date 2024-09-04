d4220e4f10238a7d6e4ba487e3bc1ea9f6123ef9
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-04 17:07:50 +0300

    "Updated styles.css with new nav and ul styles, modified app.js to use hbs and register partials, and removed about.hbs, help.hbs, and index.hbs templates."

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 const port = 8080;
 
 const publicDirectory = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectory));
 
@@ -30,6 +33,7 @@ app.get("/help", (req, res) => {
   res.render("help", {
     title: "Get Help",
     getHelp: "Visit our offices for assistance",
+    name: "Vincent Anjiri",
   });
 });
 
