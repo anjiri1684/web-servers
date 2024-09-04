474d6256331d2bb2eede6b918164bea34f1964c9
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-04 19:06:57 +0300

    "Removed contact.html, refactored CSS styles, added 404 route, and updated HTML templates for about, help, and index pages."

--- src/app.js
+++ src/app.js
@@ -37,6 +37,14 @@ app.get("/help", (req, res) => {
   });
 });
 
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    description:
+      "Sorry this page is current unavailable kindly try again later",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
